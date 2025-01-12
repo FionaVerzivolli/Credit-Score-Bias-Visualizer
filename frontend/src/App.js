6a8e2e9f180afff35268dca3dfdc5f22b5eb3cbd
FionaV2400 <139073560+FionaVerzivolli@users.noreply.github.com>
2025-01-12 03:26:06 -0500

    a

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { connectWebSocket } from "./utils/websocket"; // Import the connect function
+import { MetricsProvider } from "./MetricsContext"; // Import MetricsProvider
 import Login from "./login";
 import Upload from "./upload";
-import Home from "./home"
-import "./App.css";
+import Home from "./home";
 import Snapshots from "./snapshots";
+import "./App.css";
 
 function App() {
   useEffect(() => {
@@ -14,35 +15,35 @@ function App() {
   }, []);
 
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <div className="hero">
-                <div className="hero-content animate-fade-in-delayed">
-                  <h1 className="title">Bias Analysis in Credit Score</h1>
-                  <p>Explore potential biases and evaluate fairness metrics.</p>
-                  <div className="button-container">
-                    <Link to="/login" className="scroll-down">
-                      Get Started
-                    </Link>
+    <MetricsProvider> {/* Wrap the entire app in the MetricsProvider */}
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <div className="hero">
+                  <div className="hero-content animate-fade-in-delayed">
+                    <h1 className="title">Bias Analysis in Credit Score</h1>
+                    <p>Explore potential biases and evaluate fairness metrics.</p>
+                    <div className="button-container">
+                      <Link to="/login" className="scroll-down">
+                        Get Started
+                      </Link>
+                    </div>
                   </div>
-
                 </div>
-              </div>
-            }
-          />
-          {/* Login and Upload Routes */}
-          <Route path="/home" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/upload" element={<Upload />} />
-          <Route path="/snapshots" element={<Snapshots />} />
-
-        </Routes>
-      </div>
-    </Router>
+              }
+            />
+            {/* Login and Upload Routes */}
+            <Route path="/home" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/upload" element={<Upload />} />
+            <Route path="/snapshots" element={<Snapshots />} />
+          </Routes>
+        </div>
+      </Router>
+    </MetricsProvider>
   );
 }
 
